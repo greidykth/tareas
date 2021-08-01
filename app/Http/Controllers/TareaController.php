98a7f48cb21da79fcdcdfef53489e46833c71786
Greidy Peña <greidykp@gmail.com>
2021-08-01 18:41:32 -0500

    first commit

---
+++ app/Http/Controllers/TareaController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarea;
+use Illuminate\Http\Request;
+
+class TareaController extends Controller
+{
+    private const MESSAGE = 'Algo salió mal';
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tareas = Tarea::get();
+
+        return response()->json(['tareas' => $tareas]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $tarea = new Tarea();
+            $tarea->description = $request->description;
+            $tarea->importance = $request->importance;
+            $tarea->save();
+
+            return response()->json(['tarea' => $tarea], 201);
+
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th, 'message' => self::MESSAGE]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tarea  $tarea
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tarea $tarea)
+    {
+        return response()->json(['tarea' => $tarea]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tarea  $tarea
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tarea $tarea)
+    {
+        try {
+            $tarea->description = $request->description;
+            $tarea->importance = $request->importance;
+            $tarea->save();
+
+            return response()->json(['tarea' => $tarea], 201);
+
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th, 'message' => self::MESSAGE]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tarea  $tarea
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tarea $tarea)
+    {
+        try {
+            $tarea->delete();
+            return response()->json(['tarea' => $tarea]);
+        
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th, 'message' => self::MESSAGE]);
+        }
+    }
+}
---
+++ database/migrations/2021_08_01_225848_create_tareas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable();
+            $table->tinyInteger('importance');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tareas');
+    }
+}
